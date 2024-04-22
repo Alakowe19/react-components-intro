d5d9a38dd26ff80a74fbab0bf935a10b73ae9f24
Oluwagbemi Abiodun <oluwagbemiiabiodun@gmail.com>
2024-04-22 10:42:34 +0100

    done

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Footer from './Footer.jsx'
+import Header from './Header.jsx'
+import Main from './Main2.jsx' //I dont know if this is correct
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
